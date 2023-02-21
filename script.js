72d6b0aaf152dd70dfeebc116f7397b82b003831
SravaniPutturu <117716430+SravaniPutturu@users.noreply.github.com>
2023-02-21 21:45:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,50 @@
 //your code here!
+const child1 = document.createElement('li');
+const childText1 = document.createTextNode("ebdjeb")
+child1.appendChild(childText1);
+document.querySelector('ol').appendChild(child1).appendChild(document.createElement('br'))
 
+const child2 = document.createElement('li');
+const childText2 = document.createTextNode("ebdjeb")
+child2.appendChild(childText2);
+child1.after(child2)
+
+const child3 = document.createElement('li');
+const childText3 = document.createTextNode("ebdjeb")
+child3.appendChild(childText3);
+child2.after(child3)
+
+const child4 = document.createElement('li');
+const childText4 = document.createTextNode("ebdjeb")
+child4.appendChild(childText4);
+child3.after(child4)
+
+const child5 = document.createElement('li');
+const childText5 = document.createTextNode("ebdjeb")
+child5.appendChild(childText5);
+child4.after(child5)
+
+const child6 = document.createElement('li');
+const childText6 = document.createTextNode("ebdjeb")
+child6.appendChild(childText6);
+child5.after(child6)
+
+const child7 = document.createElement('li');
+const childText7 = document.createTextNode("ebdjeb")
+child7.appendChild(childText7);
+child6.after(child7)
+
+const child8 = document.createElement('li');
+const childText8 = document.createTextNode("ebdjeb")
+child8.appendChild(childText8);
+child7.after(child8)
+
+const child9 = document.createElement('li');
+const childText9 = document.createTextNode("ebdjeb")
+child9.appendChild(childText9);
+child8.after(child9)
+
+const child10 = document.createElement('li');
+const childText10 = document.createTextNode("ebdjeb")
+child10.appendChild(childText10);
+child9.after(child10)
